71b4e821a0bc959488843c7aa9755b0f0b236beb
月北是个程序员 <34906711+ybwork-cn@users.noreply.github.com>
2024-02-21 11:42:16 +0800

    release

---
+++ build.bat
@@ -0,0 +1,13 @@
+::设置分支名字
+SET ToolName=upm
+::设置模块源路径
+SET ToolAssetPath=Assets/ybwork/Async
+
+git branch -D %ToolName%
+git remote rm %ToolName%
+::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
+git subtree split -P %ToolAssetPath% --branch %ToolName%
+
+:: 推送到远端
+git push origin %ToolName%
+pause
