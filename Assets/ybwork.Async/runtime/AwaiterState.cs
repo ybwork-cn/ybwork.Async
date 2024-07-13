59fa28e74f02c262181fe88446986e71267bf3bf
月北是个程序员 <34906711+ybwork-cn@users.noreply.github.com>
2024-07-13 15:21:57 +0800

    整理结构

---
+++ Assets/ybwork.Async/runtime/AwaiterState.cs
@@ -0,0 +1,12 @@
+﻿// Changed by 月北(ybwork-cn) https://github.com/ybwork-cn/
+
+namespace ybwork.Async.Awaiters
+{
+    public enum AwaiterState
+    {
+        Started,
+        Completed,
+        Aborted,
+        Error,
+    }
+}
---
+++ Assets/ybwork.Async/runtime/IAwaiter.cs
@@ -0,0 +1,27 @@
+﻿// Changed by 月北(ybwork-cn) https://github.com/ybwork-cn/
+
+using System.Runtime.CompilerServices;
+
+namespace ybwork.Async.Awaiters
+{
+    public interface IAwaiter : INotifyCompletion
+    {
+        AwaiterState State { get; }
+        bool IsCompleted { get; }
+        internal void MoveNext();
+        internal void SetException();
+        void Cancel();
+    }
+
+    public interface IAwaiterVoid : IAwaiter
+    {
+        void GetResult();
+        internal void SetValue();
+    }
+
+    public interface IAwaiter<T> : IAwaiter
+    {
+        T GetResult();
+    }
+
+}
